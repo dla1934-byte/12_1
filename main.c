2420d3963d3d062e61c24b606b97371e26314fba
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-11-23 02:50:41 +0900

    1

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "board.h"
+
+
+int main(int argc, char *argv[])
+{
+  srand((unsigned)(time(NULL)));
+  
+  printf("===@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@===\n");
+  printf("          SHARK ISLAND GAME START          \n");
+  printf("===@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@===\n");
+  
+  //step 1. initialization (player name setting)
+  
+  //step 2. turn play (do-while)
+  //2-1. status printing
+  //2-2. roll dice
+  //2.3 move (result)
+  //2-4. change turn, shark move
+  
+  //step 3. game end (winner printing)
+  
+  
+  
+  
+  printf("===@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@===\n");
+  printf("           SHARK ISLAND GAME END           \n");
+  printf("===@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@===\n");
+  
+
+  
+  
+  
+  system("PAUSE");	
+  return 0;
+}
